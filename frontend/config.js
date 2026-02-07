b783febad19f5f26be0d80c8c111a8ea1187a8bf
aliiyaaa <alfiagalidullaeva@gmail.com>
2026-02-07 19:17:24 +0500

    deploy

--- frontend/config.js
+++ frontend/config.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = {
-  tokenAddress: "",
+  tokenAddress: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9",
 
-  shopAddress: "",
+  shopAddress: "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9",
   
-  crowdfundingAddress: ""
+  crowdfundingAddress: "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707"
 };
