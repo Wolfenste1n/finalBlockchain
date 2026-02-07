bf7765f74c91373d97bf7a0e6f2c01de50481814
Wolfenste1n <wolfenstrun@gmail.com>
2026-02-07 18:18:50 +0300

    fix metamask

--- frontend/config.js
+++ frontend/config.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = {
-  tokenAddress: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9",
+  tokenAddress: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
 
-  shopAddress: "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9",
+  shopAddress: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
   
-  crowdfundingAddress: "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707"
+  crowdfundingAddress: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
 };
